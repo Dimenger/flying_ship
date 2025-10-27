22dfe8c94ff4937e75962e3c888b790831e48999
Dmitry Seregin <dimenerDS@gmail.com>
2025-10-27 16:10:44 +0400

    Fixed error service no exist, spinner.

--- src/components/pages/service/service.jsx
+++ src/components/pages/service/service.jsx
@@ -5,6 +5,7 @@ import { Title } from "../../../elements/title/title";
 import { SERVICES_IMAGES } from "../../../constants/services-images";
 import { Error } from "../../error/error";
 import { ERROR } from "../../../constants";
+import { Spinner } from "../../../elements/spinner/spinner";
 
 import styles from "./services.module.css";
 
@@ -12,9 +13,10 @@ export const Service = () => {
   let param = useParams();
   const id = param.id;
 
-  const [service, setServices] = useState({});
-  const [prices, setPrices] = useState([]);
-  const [aims, setAims] = useState([]);
+  const [service, setServices] = useState(null);
+  const [prices, setPrices] = useState(null);
+  const [aims, setAims] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getService = async (id) => {
@@ -24,13 +26,18 @@ export const Service = () => {
           throw new Error(`Error, ${res.status}, ${res.statusText}`);
         }
         const services = await res.json();
+
         const service = services.find((user) => id === user.id);
+        if (!service) {
+          setLoading(false);
+          return;
+        }
         const prices = service.prices;
         const aims = service.aims;
-
         setServices(service);
         setPrices(prices);
         setAims(aims);
+        setLoading(false);
       } catch (error) {
         console.error(error);
       }
@@ -42,12 +49,15 @@ export const Service = () => {
   const getSRC = (id) => {
     const imgSrs = SERVICES_IMAGES.find((img) => id === img.id);
     if (!imgSrs) {
-      return;
+      return null;
     }
     return imgSrs.src;
   };
 
+  if (loading) return <Spinner />;
   if (!service) return <Error error={ERROR.SERVICE_NOT_EXIST} />;
+
+  console.log(prices, aims);
 
   return (
     <div className={styles.serviceContainer}>
