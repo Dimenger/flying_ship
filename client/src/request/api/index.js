f159a8c526236b03c4775d1c97894446e004cc6e
Dmitry Seregin <dimenerDS@gmail.com>
2025-11-11 13:39:04 +0400

    Client. Add modal to posts page.

--- client/src/request/api/index.js
+++ client/src/request/api/index.js
@@ -10,3 +10,4 @@ export * from "./api-add-new-post";
 export * from "./api-add-service-to-user";
 export * from "./get-user-services";
 export * from "./api-remove-service-from-user";
+export * from "./api-delete-post";
