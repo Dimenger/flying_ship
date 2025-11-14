01bd28c9432532da2fe294afe6b2e541664761df
Dmitry Seregin <dimenerDS@gmail.com>
2025-11-14 18:15:46 +0400

    Install server url

--- client/src/components/utils/request.js
+++ client/src/components/utils/request.js
@@ -1,5 +1,5 @@
 import { API_URL } from "../../constants";
 
 export const request = async (endpoint, options = {}) => {
-  return fetch(`${API_URL.LOCALHOST}${endpoint}`, options);
+  return fetch(`${API_URL.SERVER}${endpoint}`, options);
 };
