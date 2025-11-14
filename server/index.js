fae00496049da72b340fce36e93c47aae04a10f9
Dmitry Seregin <dimenerDS@gmail.com>
2025-11-14 17:28:05 +0400

    Fix for server export

--- server/index.js
+++ server/index.js
@@ -21,14 +21,14 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-// const distPath = join(__dirname, "/dist");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const distPath = join(__dirname, "/dist");
 
-// app.use(express.static(distPath));
-// app.get(/^(?!\/api).*/, (req, res) => {
-//   res.sendFile(join(distPath, "index.html"));
-// });
+app.use(express.static(distPath));
+app.get(/^(?!\/api).*/, (req, res) => {
+  res.sendFile(join(distPath, "index.html"));
+});
 
 app.use("/auth", authRouter);
 app.use("/post", postRouter);
